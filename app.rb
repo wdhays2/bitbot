c670b4fefab92b5c707352d33f0c199eb9542a2d
wdhays2 <wdhays2@charter.net>
2016-06-06 19:28:55 -0700

    added initial bot files

---
+++ app.rb
@@ -0,0 +1,24 @@
+# My Bitcoin Slack Bot
+
+require 'sinatra'
+require 'httparty'
+require 'json'
+
+post '/process_command' do
+  return if params[:token] != ENV['SLACK_TOKEN']
+
+  text = params[:text]
+  spot_price_url = 'https://api.coinbase.com/v2/prices/spot'
+
+  case text
+    when 'price'
+      resp = HTTParty.get(spot_price_url)
+      resp = JSON.parse resp.body
+      respond_message "The current Bitcoin spot price is $#{resp['data']['amount']} #{resp['data']['currency']}."
+  end
+end
+
+def respond_message message
+  content_type :json
+  {:text => message}.to_json
+end
