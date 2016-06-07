4f3458eb7544c202aabe7d03983f0de5b30dd852
wdhays2 <wdhays2@charter.net>
2016-06-06 20:46:56 -0700

    Switch back to bitcoin api.

--- app.rb
+++ app.rb
@@ -8,13 +8,13 @@ post '/price' do
   return if params[:token] != ENV['SLACK_TOKEN']
 
   text = params[:text]
-  spot_price_url = 'http://www.omdbapi.com/?t=%22Jurassic%20Park%22'
+  spot_price_url = 'https://api.coinbase.com/v2/prices/spot'
 
   case text
     when 'price'
       resp = HTTParty.get(spot_price_url)
       resp = JSON.parse resp.body
-      respond_message "#{resp}"
+      respond_message "The current Bitcoin spot price is $#{resp['data']['amount']} #{resp['data']['currency']}."
   end
 end
 
